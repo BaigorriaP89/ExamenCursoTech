6645a8bd94b1c31374db1560c47eacfec2207155
Pablo <pablo.baigorria_89@hotmail.com>
2024-12-14 13:15:09 -0300

    se agregan todos los likes

--- scrip.js
+++ scrip.js
@@ -4,12 +4,29 @@ console.log(document.getElementById("contador").textContent)// DEVUELVE EL ATRIB
 console.log(document.getElementById("contador").textContent = "558")// DEVUELVE EL VALOR */
 
 var validadorLike = false;
-var contadorMeGusta = 0;
+var contadorMeGusta = 1500;
+var validadorLike2 = false;
+var contadorMeGusta2 = 17800;
+var validadorLike3 = false;
+var contadorMeGusta3 = 57428;
+var validadorLike4 = false;
+var contadorMeGusta4 = 1780;
+var validadorLike5 = false;
+var contadorMeGusta5 = 17880;
+var validadorLike6 = false;
+var contadorMeGusta6 = 50742;
+var validadorLike7 = false;
+var contadorMeGusta7 = 98561;
+var validadorLike8 = false;
+var contadorMeGusta8 = 98561;
+var validadorLike9 = false;
+var contadorMeGusta9 = 98561;
+
 function contador(){
     console.log("Esta funcion funciona")
 
-   console.log(document.getElementById("contador"))
-   var contador =document.getElementById("contador");
+   console.log(document.getElementById("contador1"))
+   var contador =document.getElementById("contador1");
    
     if(validadorLike){
  
@@ -31,6 +48,262 @@ function contador(){
 
 }
 
-document.getElementById("count").onclick = function (){
+document.getElementById("count1").onclick = function (){
     contador();
 }
+
+function contador2(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador2"))
+   var contador =document.getElementById("contador2");
+   
+    if(validadorLike2){
+ 
+        validadorLike2 = false;
+        contadorMeGusta2 = contadorMeGusta2 - 1;
+              
+        
+        contador.textContent = contadorMeGusta2
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta2)
+        
+    } else{
+        validadorLike2 = true;
+        
+        contadorMeGusta2 ++;
+        contador.textContent = contadorMeGusta2
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta2)
+    }
+
+
+}
+
+document.getElementById("count2").onclick = function (){
+    
+    contador2();
+}
+
+//---------------------------------
+
+function contador3(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador3"))
+   var contador =document.getElementById("contador3");
+   
+    if(validadorLike3){
+ 
+        validadorLike3 = false;
+        contadorMeGusta3= contadorMeGusta3 - 1;
+              
+        
+        contador.textContent = contadorMeGusta3
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta3)
+        
+    } else{
+        validadorLike3 = true;
+        
+        contadorMeGusta3 ++;
+        contador.textContent = contadorMeGusta3
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta3)
+    }
+
+
+}
+
+document.getElementById("count3").onclick = function (){
+    contador3();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador4(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador4"))
+   var contador =document.getElementById("contador4");
+   
+    if(validadorLike4){
+ 
+        validadorLike4 = false;
+        contadorMeGusta4 = contadorMeGusta4 - 1;
+              
+        
+        contador.textContent = contadorMeGusta4
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta4)
+        
+    } else{
+        validadorLike4 = true;
+        
+        contadorMeGusta4 ++;
+        contador.textContent = contadorMeGusta4
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta4)
+    }
+
+
+}
+
+document.getElementById("count4").onclick = function (){
+    contador4();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador5(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador5"))
+   var contador =document.getElementById("contador5");
+   
+    if(validadorLike5){
+ 
+        validadorLike5 = false;
+        contadorMeGusta5 = contadorMeGusta5 - 1;
+              
+        
+        contador.textContent = contadorMeGusta5
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta5)
+        
+    } else{
+        validadorLike5 = true;
+        
+        contadorMeGusta5 ++;
+        contador.textContent = contadorMeGusta5
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta5)
+    }
+
+
+}
+
+document.getElementById("count5").onclick = function (){
+    contador5();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador6(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador6"))
+   var contador =document.getElementById("contador6");
+   
+    if(validadorLike6){
+ 
+        validadorLike = false;
+        contadorMeGusta6 = contadorMeGusta6 - 1;
+              
+        
+        contador.textContent = contadorMeGusta6
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta6)
+        
+    } else{
+        validadorLike6 = true;
+        
+        contadorMeGusta6 ++;
+        contador.textContent = contadorMeGusta6
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta6)
+    }
+
+
+}
+
+document.getElementById("count6").onclick = function (){
+    contador6();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador7(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador7"))
+   var contador =document.getElementById("contador7");
+   
+    if(validadorLike7){
+ 
+        validadorLike7 = false;
+        contadorMeGusta7 = contadorMeGusta7 - 1;
+              
+        
+        contador.textContent = contadorMeGusta7
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta7)
+        
+    } else{
+        validadorLike7 = true;
+        
+        contadorMeGusta7 ++;
+        contador.textContent = contadorMeGusta7
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta7)
+    }
+
+
+}
+
+document.getElementById("count7").onclick = function (){
+    contador7();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador8(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador8"))
+   var contador =document.getElementById("contador8");
+   
+    if(validadorLike8){
+ 
+        validadorLike8 = false;
+        contadorMeGusta8 = contadorMeGusta8 - 1;
+              
+        
+        contador.textContent = contadorMeGusta8
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta8)
+        
+    } else{
+        validadorLike8 = true;
+        
+        contadorMeGusta8 ++;
+        contador.textContent = contadorMeGusta8
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta8)
+    }
+
+
+}
+
+document.getElementById("count8").onclick = function (){
+    contador8();
+}
+//-----------------------------------------
+//---------------------------------
+
+function contador9(){
+    console.log("Esta funcion funciona")
+
+   console.log(document.getElementById("contador9"))
+   var contador =document.getElementById("contador9");
+   
+    if(validadorLike9){
+ 
+        validadorLike9 = false;
+        contadorMeGusta9 = contadorMeGusta9 - 1;
+              
+        
+        contador.textContent = contadorMeGusta9
+        console.log("Validador verdadero y el valor del contador es " + contadorMeGusta9)
+        
+    } else{
+        validadorLike9 = true;
+        
+        contadorMeGusta9 ++;
+        contador.textContent = contadorMeGusta9
+        console.log("Validador falso y el valor del contador es " + contadorMeGusta9)
+    }
+
+
+}
+
+document.getElementById("count9").onclick = function (){
+    contador9();
+}
+//-----------------------------------------
